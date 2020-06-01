f0a6bae389aef49d0af4d3a1b4c14a920447b828
Jef Dewitt <jef.dewitt@twinspires.com>
2020-06-01 19:42:54 -0400

    Initial commit -- compeleted class project.

---
+++ cmake-build-debug/CMakeFiles/Lab4_2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab4_2.dir/CSVparser.cpp.o"
+  "CMakeFiles/Lab4_2.dir/VectorSorting.cpp.o"
+  "Lab4_2"
+  "Lab4_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab4_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
